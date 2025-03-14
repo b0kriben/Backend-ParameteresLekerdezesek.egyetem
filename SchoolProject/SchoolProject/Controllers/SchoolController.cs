79aa884cfa396e6cccd96d180838e71ae9f6407d
2020b-Krisztin Benedek <144203768+b0kriben@users.noreply.github.com>
2025-03-14 11:36:11 +0100

    első feladat

---
+++ SchoolProject/SchoolProject/Controllers/SchoolController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolProject.Models;
+
+namespace SchoolProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolController : ControllerBase
+    {
+        private readonly SchoolContext _context = new SchoolContext();
+
+        [HttpGet("students")]
+        public async Task<IActionResult> GetAllStudentAsync()
+        {
+            return Ok(await _context.Students.ToListAsync());
+        }
+
+        [HttpGet("departments")]
+        public async Task<IActionResult> GetAllDepartmentAsync()
+        {
+            return Ok(await _context.Departments.ToListAsync());
+        }
+
+        [HttpGet("studentsOfDepartment")]
+        public async Task<IActionResult> GetStudentsOfDepartmentAsync([FromQuery] string departmentName)
+        {
+            var result = (from s in _context.Students
+                         from d in _context.Departments
+                         where s.DepartmentId == d.Id && d.Name == departmentName
+                         select s.Name).ToListAsync();
+            return Ok(await result);
+        }
+    }
+}
---
+++ SchoolProject/SchoolProject/Models/SchoolContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolProject.Models;
+
+public partial class SchoolContext : DbContext
+{
+    public SchoolContext()
+    {
+    }
+
+    public SchoolContext(DbContextOptions<SchoolContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("Data Source=Database\\school.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("department");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("INT")
+                .HasColumnName("ID");
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("student");
+
+            entity.Property(e => e.Age).HasColumnType("INT");
+            entity.Property(e => e.DepartmentId)
+                .HasColumnType("INT")
+                .HasColumnName("DepartmentID");
+            entity.Property(e => e.Enrolled)
+                .HasColumnType("INT")
+                .HasColumnName("Enrolled ");
+            entity.Property(e => e.Id)
+                .HasColumnType("INT")
+                .HasColumnName("ID");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
