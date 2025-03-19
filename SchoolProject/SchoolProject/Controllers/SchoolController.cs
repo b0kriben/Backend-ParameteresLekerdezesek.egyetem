0c89550f515a1cb8b538c07a2fcb22aff0556bfb
2020b-Krisztin Benedek <144203768+b0kriben@users.noreply.github.com>
2025-03-19 11:33:43 +0100

    Add files via upload

--- SchoolProject/SchoolProject/Controllers/SchoolController.cs
+++ SchoolProject/SchoolProject/Controllers/SchoolController.cs
@@ -26,11 +26,67 @@ namespace SchoolProject.Controllers
         [HttpGet("studentsOfDepartment")]
         public async Task<IActionResult> GetStudentsOfDepartmentAsync([FromQuery] string departmentName)
         {
-            var result = (from s in _context.Students
+            // 1. query format
+            var result1 = from s in _context.Students
                          from d in _context.Departments
                          where s.DepartmentId == d.Id && d.Name == departmentName
-                         select s.Name).ToListAsync();
-            return Ok(await result);
+                         select s.Name;
+
+            // 2. method format
+            var result = _context.Students
+                        .Join(_context.Departments, s => s.DepartmentId, d => d.Id, (s, d) => new { Student = s, Department = d })
+                        .Where(sd => sd.Department.Name == departmentName)
+                        .Select(sd => sd.Student.Name);
+            return Ok(result.ToListAsync());
+        }
+
+        //4. Endpoint: `GET /departments/{department_id}/students/count`  
+        //Feladat: Készíts egy végpontot, amely visszaadja egy adott tanszékhez tartozó diákok számát.
+        [HttpGet("/departments /{department_id}/students/count")]
+        public async Task<IActionResult> GetNumberOfStudentByDepartment(int department_id)
+        {
+            return Ok(await _context.Students.Where(s => s.DepartmentId == department_id).CountAsync());
+        }
+
+        // Többtánlás változat
+        [HttpGet("/departments/students/count")]
+        public async Task<IActionResult> GetNumberOfStudentByDepartment([FromQuery] string departmentName)
+        {
+            // 1. query format
+            var result1 = from s in _context.Students
+                         from d in _context.Departments
+                         where s.DepartmentId == d.Id && d.Name == departmentName
+                         select s.Name;
+
+            // 2. method format
+            var result = _context.Students
+                        .Join(_context.Departments, s => s.DepartmentId, d => d.Id, (s, d) => new { Student = s, Department = d })
+                        .Where(sd => sd.Department.Name == departmentName)
+                        .Select(sd => sd.Student.Name);
+            return Ok(result.ToListAsync());
+        }
+
+        //Projekciós feladatok
+        //1. Endpoint: `GET /students`  
+        //Feladat: Készíts egy végpontot, amely visszaadja az összes diák nevét.
+        [HttpGet("/studentsName")]
+        public async Task<IActionResult> GetStudentsName()
+        {
+            return Ok(await _context.Students.Select(s => s.Name).ToListAsync());
+        }
+
+        //3. Endpoint: `GET /students`  
+        //Feladat: Készíts egy végpontot, amely visszaadja az összes diák nevét és email címét.
+        [HttpGet("/studentsNameAndEmail")]
+        public async Task<IActionResult> GetStudentsNameAndEmail()
+        {
+            return Ok(await _context.Students.Select(s => new {s.Name, s.Email}).ToListAsync());
+        }
+
+        [HttpGet("/studentsNameAndEmail2")]
+        public async Task<IActionResult> GetStudentsNameAndEmail2()
+        {
+            return Ok(await _context.Students.Select(s => new NameAndEmail{Name = s.Name, Email = s.Email}).ToListAsync());
         }
     }
 }
---
+++ SchoolProject/SchoolProject/Models/NameAndEmail.cs
@@ -0,0 +1,14 @@
+﻿namespace SchoolProject.Models
+{
+    public class NameAndEmail
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        /*public NameAndEmail(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }*/
+    }
+}
